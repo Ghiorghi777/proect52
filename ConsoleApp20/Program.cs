3465c06f1fa916e902b30ef7549bdc4a7ed80bfb
Ghiorghi777 <goncargeorgij9@gmail.com>
2024-12-10 18:41:46 +0200

    Add files via upload

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+    
+        // Статический метод, который вычисляет количество сантиметров
+        public static int Centimeters(int m, int c)
+        {
+            return (m * 100) + c;  // Преобразуем метры в сантиметры и добавляем дополнительные сантиметры
+        }
+
+        static void Main(string[] args)
+        {
+            // Пример а) 2 м 4 см
+            int result1 = Centimeters(2, 4);
+            Console.WriteLine("Количество сантиметров для 2 м 4 см: " + result1);  // Ожидаемый результат: 204
+
+            // Пример б) 15 м 27 см
+            int result2 = Centimeters(15, 27);
+            Console.WriteLine("Количество сантиметров для 15 м 27 см: " + result2);  // Ожидаемый результат: 1527
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ пр.7 гр115.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Введите ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine(name); */
+
+            // б) Запрашиваем имя и повторяем его с приветствием
+            /* Console.WriteLine("Введите ваше имя:");
+             string name = Console.ReadLine();
+             Console.WriteLine($"Привет,{name}!");
+             Console.Read(); */
+
+            //Console.Write("Ведите название командвы:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"{name}-это чемпионы");
+            //Console.Read();
+
+
+
+            //Console.WriteLine("как тебя зовут?");
+            //string firstName = Console.ReadLine();
+            //Console.WriteLine("какая твоя фамилия?");
+            //string lastName = Console.ReadLine();
+            //Console.Write($"Тебя зовут: {firstName} {lastName}");
+            //Console.Read();
+
+            //Console.WriteLine("название сталица:");
+            //string capital = Console.ReadLine();
+            //Console.WriteLine("название страны:");
+            //string country = Console.ReadLine();
+            //Console.Write($"столица государства:{country}; город: {capital}");
+            //Console.Read();
+
+
+
+
+            //Console.Write("название романа:");
+            //string name = Console.ReadLine();
+            //Console.Write("автор романа:");
+            //string author = Console.ReadLine();
+            //Console.Write($"название романа: '{name}'; автор романа {author}");
+            //Console.Read();
+
+
+            //Console.WriteLine("Введите название клуба");
+            //string name = Console.ReadLine();
+            //Console.Write($"символов: {name.Length} ");
+            //Console.Read();
+
+
+
+
+
+
+        }
+    }
+}
